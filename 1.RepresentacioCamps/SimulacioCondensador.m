caa8754fa4f5923f9a9770db3146443d1c07e1d3
sarasame00 <82454961+sarasame00@users.noreply.github.com>
2024-05-14 11:06:53 +0200

    Update SimulacioCondensador.m

--- 1.RepresentacioCamps/SimulacioCondensador.m
+++ 1.RepresentacioCamps/SimulacioCondensador.m
@@ -1,29 +1,26 @@
 elecmodel = createpde("electromagnetic","electrostatic");
 
-%fons
-fons = [2 4 0 280 280 0 0 0 200 200];
+x1 = [108, 112, 112, 108];
+y1 = [50, 50, 150, 150];
+x2 = [168, 172, 172, 168];
+y2 = [50, 50, 150, 150];
+x3 = [0 ,280, 280, 0];
+y3 = [0, 0, 200, 200];
 
-%triangles exteriors
-t1 = [2 3 70 210 140 40 40 160 0 0];
-t2 = [2 3 77 203 140 44 44 153 0 0];
+p1 = [2 4 x1 y1];
+p2 = [2 4 x2 y2];
+p3 = [2 4 x3 y3];
+gd = [p1' p2' p3'];
 
-%triangles interiors
-t3 = [2 3 115 165 140 93 93 50 0 0];
-t4 = [2 3 120 160 140 90 90 55 0 0];
-gd = [fons' t1' t2' t3' t4'];
-
-d = decsg(gd,'fons-(t1-t2)-(t3-t4)',char('fons','t1','t2','t3','t4')');
+d = decsg(gd,'P3-P1-P2',char('P1','P2','P3')');
 
 geometryFromEdges(elecmodel,d);
-
-figure
-pdegplot(elecmodel,'EdgeLabels','on','FaceLabels','on');
 
 elecmodel.VacuumPermittivity = 8.8541878128E-12;
 electromagneticProperties(elecmodel,"RelativePermittivity",1.2); 
 
-electromagneticBC(elecmodel,"Voltage", 10, "Edge",[3, 4, 14, 15, 10, 11]);
-electromagneticBC(elecmodel,"Voltage", 0, "Edge",[13, 12, 5, 6, 16, 7]);
+electromagneticBC(elecmodel,"Voltage", 10, "Edge",[6, 9, 10, 7]);
+electromagneticBC(elecmodel,"Voltage", 0, "Edge",[1, 11, 12, 8]);
 
 model = generateMesh(elecmodel); 
 
@@ -33,10 +30,10 @@ u = R.ElectricPotential;
 
 figure
 pdeplot(elecmodel, ...
-    XYData = u, ...
-    FlowData = [R.ElectricField.Ex R.ElectricField.Ey], ...
-    Contour = "on", ...
-    Levels = 10, ...
-    ColorMap = "turbo")
+    XYData=u, ...
+    FlowData=[R.ElectricField.Ex R.ElectricField.Ey], ...
+    Contour="on", ...
+    Levels=20, ...
+    ColorMap="turbo")
 axis([0 280 0 200])
 set(gca,'DataAspectRatio',[1 1 1])
